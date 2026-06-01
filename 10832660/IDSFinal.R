bd97d38d6e11b6fead4d887e4599831e6236c129
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-06-01 19:56:29 +0000

    Review: IDSFinal.R - 2026-06-01 [1/9]

--- 10832660/IDSFinal.R
+++ 10832660/IDSFinal.R
@@ -4,3 +4,4 @@ tally(~ How.much.time.do.you.spend.using.social.media.., data = TimeSpentOnSocia
 
 bargraph(~ What.is.the.most.used.social.media.app.in.you.phone., data = TimeSpentOnSocialMedia, main = "Most Used Social Media App", xlab = "App", ylab = "Number of Students", col = "lightyellow")
 tally(~ What.is.the.most.used.social.media.app.in.you.phone., data = TimeSpentOnSocialMedia)
+# reviewed: 2026-06-01 19:56:29 [1] run-7422
