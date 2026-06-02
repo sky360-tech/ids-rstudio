cc20204769e7fd887510b282cf80b2cd16b02bbb
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-06-02 18:43:10 +0000

    Review: Lab2Faculty.R - 2026-06-02 [8/9]

--- 10832660/Lab2Faculty.R
+++ 10832660/Lab2Faculty.R
@@ -23,3 +23,4 @@ tally(~nfemale, data = faculty1, format = "percent")
 histogram(~nfemale, data = faculty1, nint = 3)
 
 # reviewed: 2026-05-28 18:17:49 [6] run-6249
+# reviewed: 2026-06-02 18:43:10 [8] run-8364
