f0371695817ef183fb6805fb5210e25e9da1fb2c
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-06-06 16:09:27 +0000

    Review: Lab1C.R - 2026-06-06 [8/9]

--- 10832660/Lab1C.R
+++ 10832660/Lab1C.R
@@ -2,3 +2,4 @@ load_labs(“1C”)
 histogram(~ calories, data = food)
 # reviewed: 2026-05-27 23:14:42 [4] run-2986
 # reviewed: 2026-06-01 19:56:29 [6] run-23154
+# reviewed: 2026-06-06 16:09:27 [8] run-6396
