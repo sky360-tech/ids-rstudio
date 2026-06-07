bc58a53da4bd7ea89ea7ff5723f0143369326bfc
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-06-07 16:20:13 +0000

    Review: Lab2E.R - 2026-06-07 [8/9]

--- 10832660/Lab2E.R
+++ 10832660/Lab2E.R
@@ -44,3 +44,4 @@ histogram(~diff,data=shuffled_gender,fit="normal")
 add_line(vline = 22.52252 - 13.30798)
 # reviewed: 2026-05-28 18:17:49 [4] run-1028
 # reviewed: 2026-06-02 18:43:10 [6] run-24153
+# reviewed: 2026-06-07 16:20:13 [8] run-14885
