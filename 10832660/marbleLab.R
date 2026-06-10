652b42d0c1f2beb99f61aa1cf5b2e45f1c8e6a97
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-06-10 18:05:59 +0000

    Review: marbleLab.R - 2026-06-10 [3/9]

--- 10832660/marbleLab.R
+++ 10832660/marbleLab.R
@@ -12,3 +12,4 @@ marble_draw <- do(2000) * sample(marbles, size = 1, replace = TRUE)
 tally(~sample,data=marble_draw)/2000
 tally(~sample,data=marble_draw,format="proportion")# reviewed: 2026-05-30 16:07:17 [8] run-4221
 # reviewed: 2026-06-05 17:21:51 [1] run-20680
+# reviewed: 2026-06-10 18:05:59 [3] run-11946
