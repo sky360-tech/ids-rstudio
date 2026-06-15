4c7423646ddd10bf9d69afbdbab49ca048370bdf
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-06-15 19:05:14 +0000

    Review: marbleLab.R - 2026-06-15 [5/9]

--- 10832660/marbleLab.R
+++ 10832660/marbleLab.R
@@ -13,3 +13,4 @@ tally(~sample,data=marble_draw)/2000
 tally(~sample,data=marble_draw,format="proportion")# reviewed: 2026-05-30 16:07:17 [8] run-4221
 # reviewed: 2026-06-05 17:21:51 [1] run-20680
 # reviewed: 2026-06-10 18:05:59 [3] run-11946
+# reviewed: 2026-06-15 19:05:14 [5] run-10665
