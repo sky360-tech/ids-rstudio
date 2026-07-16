55a6739eb9a5aa7984804500cf51cc28021adadc
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-07-16 16:19:11 +0000

    Review: cookies.R - 2026-07-16 [5/9]

--- 10832660/cookies.R
+++ 10832660/cookies.R
@@ -45,3 +45,4 @@ tally(~nmale,data=new_litters2000,format="proportion")
 # reviewed: 2026-06-30 17:07:42 [8] run-728
 # reviewed: 2026-07-06 17:45:42 [1] run-28672
 # reviewed: 2026-07-11 15:59:07 [3] run-929
+# reviewed: 2026-07-16 16:19:11 [5] run-19217
