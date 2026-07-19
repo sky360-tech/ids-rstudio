da305d652f8a500da0001df143a0587d392b3231
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-07-19 15:59:52 +0000

    Review: Lab2Faculty.R - 2026-07-19 [8/9]

--- 10832660/Lab2Faculty.R
+++ 10832660/Lab2Faculty.R
@@ -32,3 +32,4 @@ histogram(~nfemale, data = faculty1, nint = 3)
 # reviewed: 2026-07-04 16:06:21 [2] run-6295
 # reviewed: 2026-07-09 17:23:11 [4] run-20894
 # reviewed: 2026-07-14 16:18:47 [6] run-23314
+# reviewed: 2026-07-19 15:59:52 [8] run-17375
