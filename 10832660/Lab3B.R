ec2635f014fe1282e34e1026c7d20421d50a32ee
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-07-25 16:06:04 +0000

    Review: Lab3B.R - 2026-07-25 [6/9]

--- 10832660/Lab3B.R
+++ 10832660/Lab3B.R
@@ -42,3 +42,4 @@ sample(1:21, size = 10, replace = FALSE)
 # reviewed: 2026-07-09 17:23:11 [9] run-2985
 # reviewed: 2026-07-15 16:25:57 [2] run-24565
 # reviewed: 2026-07-20 16:26:09 [4] run-23746
+# reviewed: 2026-07-25 16:06:04 [6] run-16704
