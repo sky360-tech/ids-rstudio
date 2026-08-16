7c6137da84a3bb43733694957222c6c5a3e7f927
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-08-16 15:24:53 +0000

    Review: Lab2G.R - 2026-08-16 [1/9]

--- 10832660/Lab2G.R
+++ 10832660/Lab2G.R
@@ -28,3 +28,4 @@ bwplot(~stress|p_color,data=stress_colors)
 # reviewed: 2026-07-30 16:37:55 [4] run-5867
 # reviewed: 2026-08-04 16:54:39 [6] run-21965
 # reviewed: 2026-08-10 16:01:31 [8] run-2021
+# reviewed: 2026-08-16 15:24:53 [1] run-16927
