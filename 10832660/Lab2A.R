be07a3d2111b238689530d37c1d2d7638cb81ea3
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-08-20 15:36:21 +0000

    Review: Lab2A.R - 2026-08-20 [6/9]

--- 10832660/Lab2A.R
+++ 10832660/Lab2A.R
@@ -59,3 +59,4 @@ dotPlot(~gold|int_ext,data=personalityColor,nint=13,cex=.5,layout=c(1,2))
 # reviewed: 2026-08-03 17:10:45 [9] run-16337
 # reviewed: 2026-08-10 16:01:31 [2] run-7654
 # reviewed: 2026-08-15 15:24:25 [4] run-23130
+# reviewed: 2026-08-20 15:36:21 [6] run-22895
