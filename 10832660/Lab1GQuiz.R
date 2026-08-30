93e2eb182759da8d4902b8373d8c7258d58cb1f9
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-08-30 18:29:43 +0000

    Review: Lab1GQuiz.R - 2026-08-30 [8/9]

--- 10832660/Lab1GQuiz.R
+++ 10832660/Lab1GQuiz.R
@@ -51,3 +51,4 @@ tally(~depressed|sex,data=cdc,format="percent")
 # reviewed: 2026-08-15 15:24:25 [2] run-16949
 # reviewed: 2026-08-20 15:36:21 [4] run-2210
 # reviewed: 2026-08-25 15:45:15 [6] run-11264
+# reviewed: 2026-08-30 18:29:43 [8] run-29830
