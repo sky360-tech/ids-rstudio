d1dade4375379bc1a0e09a390e83a390fdf10594
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-09-16 18:44:48 +0000

    Review: Lab3A.R - 2026-09-16 [7/9]

--- 10832660/Lab3A.R
+++ 10832660/Lab3A.R
@@ -42,3 +42,4 @@ p_value <- 1 - pnorm(actual_diff, mean = diff_mean, sd = diff_sd)
 # reviewed: 2026-09-01 18:15:15 [1] run-15032
 # reviewed: 2026-09-06 17:33:01 [3] run-19271
 # reviewed: 2026-09-11 18:16:30 [5] run-17076
+# reviewed: 2026-09-16 18:44:48 [7] run-15269
