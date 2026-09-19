3e33db3894c3c5c9b12cb1349ad34850e64fc547
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-09-19 17:50:36 +0000

    Review: Lab 2H.R - 2026-09-19 [9/9]

--- 10832660/Lab 2H.R
+++ 10832660/Lab 2H.R
@@ -55,3 +55,4 @@ histogram(~diff,data=shuffled_survival_mean,fit="Normal")
 # reviewed: 2026-09-04 18:11:32 [3] run-24070
 # reviewed: 2026-09-09 18:22:56 [5] run-29852
 # reviewed: 2026-09-14 19:45:40 [7] run-18818
+# reviewed: 2026-09-19 17:50:36 [9] run-162
