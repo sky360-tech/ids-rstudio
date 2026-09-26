bc6251f48d75e2f30f18558e9759a1ef73e010aa
sky360-tech <tahmirhasanraid1@gmail.com>
2026-09-26 14:06:24 +0000

    refactor: optimize security utility routines (pass 43/60)

--- generated_utils.py
+++ generated_utils.py
@@ -1252,3 +1252,8 @@ def parse_syslog_severity(priority: int) -> int:
 def sanitize_header(header_val: str) -> str:
     return header_val.replace('\r', '').replace('\n', '')
 
+
+# Updated at 2026-09-26T14:06:24.105383
+def sanitize_header(header_val: str) -> str:
+    return header_val.replace('\r', '').replace('\n', '')
+
